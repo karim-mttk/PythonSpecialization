1f3e28db1f5cd0e10462972431da7a2d3f181549
karim-mttk <muttakin.karim@gmail.com>
2023-05-21 21:00:15 +0200

    new problems

---
+++ MutateLists&Strings.py
@@ -0,0 +1,8 @@
+#problem 1
+sports = ['cricket', 'football', 'volleyball', 'baseball', 'softball', 'track and field', 'curling', 'ping pong', 'hockey']
+sports.insert(2, 'horseback riding')
+
+#problem 2
+trav_dest = ['Beirut', 'Milan', 'Pittsburgh', 'Buenos Aires', 'Nairobi', 'Kathmandu', 'Osaka', 'London', 'Melbourne']
+trav_dest.remove('London')
+print(trav_dest)

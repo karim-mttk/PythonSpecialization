5fe0ecb7ae6348d1f8250387de0eddfe7b4892db
karim-mttk <muttakin.karim@gmail.com>
2023-06-04 15:51:20 +0200

    new commits

--- InternetAPIs.py
+++ InternetAPIs.py
@@ -14,3 +14,13 @@ print(x[0])
 print("---the whole list, pretty printed---")
 print(json.dumps(x, indent=2)) # pretty print the results
 
+#problem 2:
+import requests_with_caching
+import json
+
+parameters = {"term": "Ann Arbor", "entity": "podcast"}
+iTunes_response = requests_with_caching.get("https://itunes.apple.com/search", params = parameters, permanent_cache_file="itunes_cache.txt")
+
+py_data = json.loads(iTunes_response.text)
+for r in py_data['results']:
+    print(r['trackName'])

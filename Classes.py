dcc403050ba3d70be31da5f441ff7410001d46d8
karim-mttk <muttakin.karim@gmail.com>
2023-06-07 22:15:15 +0200

    new commits

--- Classes.py
+++ Classes.py
@@ -47,3 +47,32 @@ c2 = Cereal("Honey Nut Cheerios", "General Mills", 3)
 
 print(c1)
 print(c2)
+
+#problem 5:
+class AppleBasket():
+    def __init__(self, apple_color, apple_quantity):
+        self.apple_color = apple_color
+        self.apple_quantity = apple_quantity
+
+    def increase(self):
+        self.apple_quantity += 1
+
+    def __str__(self):
+        return 'A basket of {} {} apples.'.format(self.apple_quantity, self.apple_color)
+
+a1 = AppleBasket('green', 30)
+AppleBasket.increase(a1)
+print(a1)
+
+#problem 6:
+class BankAccount():
+    def __init__(self, name, amt):
+        self.name = name
+        self.amt = amt
+
+    def __str__(self):
+        return 'Your account, {}, has {} dollars.'.format(self.name, self.amt)
+
+t1 = BankAccount('Bob', 100)
+print(t1)
+
--- Datastructures.py
+++ Datastructures.py
@@ -4,3 +4,42 @@ pos=text.find('.')
 num = text[pos:pos+5]
 print(float(num))
 
+#problem 2:
+fname = input("Enter file name: ")
+
+with open(fname, 'r') as fl:
+    content = fl.read()
+    content = content.rstrip()
+
+mod_con = content.upper()
+
+print(mod_con)
+
+#problem 3:
+def extract(line):
+    words = line.split()  # Split the line into individual words
+    for word in words:
+        try:
+            float_num = float(word)  # Attempt to convert each word to a float
+            return float_num
+        except ValueError:
+            pass  # Ignore words that cannot be converted to floats
+
+    return None
+
+
+fname = input("Enter file name: ")
+fh = open(fname)
+nums = []
+for line in fh:
+    if line.startswith("X-DSPAM-Confidence:"):
+        nums.append(extract(line))
+
+avg = 0
+s = 0
+n = len(nums)
+for i in nums:
+    s += i
+avg = round((s / n), 16)
+
+print('Average spam confidence:', avg)

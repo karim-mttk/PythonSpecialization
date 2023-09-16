0001fe116d4fd5c275e0fb4aaf83d7b38461df17
karim-mttk <muttakin.karim@gmail.com>
2023-09-16 14:26:17 +0200

    updates

---
+++ ObjectOrientedProgramming/oops_basics_11.py
@@ -0,0 +1,9 @@
+from ObjectOrientedProgramming.oops_basics_10 import My_Calc
+
+calc1 = My_Calc(105, 270)
+total1 = calc1.total()
+diff1 = calc1.diff()
+
+print(f"Total1: {total1}")
+print(f"Diff1: {diff1}")
+

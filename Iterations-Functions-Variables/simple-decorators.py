e18e10f04b3db580d3597dcda6e89e22ec3e1509
karim-mttk <muttakin.karim@gmail.com>
2023-10-26 17:46:35 +0200

    updates

--- Iterations-Functions-Variables/simple-decorators.py
+++ Iterations-Functions-Variables/simple-decorators.py
@@ -1,23 +1,33 @@
 import functools
-user = {"username": "Anna", "access_level": "guest"}
 
 
-def make_secure(func):
-    @functools.wraps(func)
-    def secure_function():
-        if user["access_level"] == "admin":
-            return func()
-        else:
-            return f"No admin permissions for {user['username']}."
+def make_secure(access_level):
+    def decorator(func):
+        @functools.wraps(func)
+        def secure_function(*args, **kwargs):
+            if user["access_level"] == access_level:
+                return func(*args, **kwargs)
+            else:
+                return f"No {access_level} permissions for {user['username']}."
+        return secure_function
+    return decorator
 
-    return secure_function
 
-
-@make_secure
+@make_secure("admin")
 def get_admin_password():
-    return "1234"
+    return "admin: 1234"
 
 
+@make_secure("user")
+def get_user_password():
+    return "user: user_password"
+
+
+user = {"username": "Anna", "access_level": "admin"}
 print(get_admin_password())
+print(get_user_password())
 
+user = {"username": "Bob", "access_level": "user"}
+print(get_admin_password())
+print(get_user_password())
 
